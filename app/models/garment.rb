41e5eecac231090f2097ebb08248b479f20ed9f1
Brian Flynn <bdflynny@gmail.com>
2018-03-30 02:27:27 -0400

    update styles for phones

--- app/models/garment.rb
+++ app/models/garment.rb
@@ -3,6 +3,7 @@ class Garment < ApplicationRecord
   has_many :measurements, dependent: :destroy
 
   validates :title, presence: true, allow_blank: false
+  validates :image_url, presence: true, allow_blank: false
 
   mount_uploader :image_url, ImageUploader
 end
