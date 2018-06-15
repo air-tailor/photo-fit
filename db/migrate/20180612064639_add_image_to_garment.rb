0eb28ea7553260bed903dbcc72c1c9792059ff72
briandangerflynn <bdflynny@gmail.com>
2018-06-15 19:44:44 -0400

    remove migrations that had to do with paperclip

--- db/migrate/20180612064639_add_image_to_garment.rb
+++
@@ -1,9 +0,0 @@
-class AddImageToGarment < ActiveRecord::Migration[5.1]
-    def self.up
-      add_attachment :garments, :image
-    end
-
-    def self.down
-      remove_attachment :garments, :image
-    end
-end
